a249916c4fdca967fad58c74fcf8912e94927c97
wuhui <56593113@qq.com>
2014-04-17 23:47:59 +0800

    tree

--- tree.cpp
+++ tree.cpp
@@ -33,13 +33,101 @@ TREE_NODE *creat_tree_node(int data)
 	return p;
 }
 
+int _insert_node_to_tree(TREE_NODE * &head_node, int data, TREE_NODE *parent)
+{
+    if (head_node == NULL)
+    {
+    	TREE_NODE *p = creat_tree_node(data);
+        CHK(p != NULL);
+        head_node = p;
+        p->parent = parent;
+        return 0;
+
+    }
+
+    if (data < head_node->data)
+    {
+        _insert_node_to_tree(head_node->left_child, data, head_node);
+    }
+    else 
+    {
+        _insert_node_to_tree(head_node->right_child, data, head_node);
+    }
+    return 0;
+}
+
 int insert_node_to_tree(TREE_NODE * &head_node, int data)
 {
-	TREE_NODE *p = creat_tree_node(data);
+    if (head_node == NULL)
+    {
+    	TREE_NODE *p = creat_tree_node(data);
+        CHK(p != NULL);
 
+        head_node = p;
+        return 0;
+    }
+
+    
 	return 0;
 }
+
+int find_node_in_tree(TREE_NODE* &head_node, int data, TREE_NODE *&node)
+{
+    if (head_node == NULL)
+    {
+        node = NULL;
+        return 0;
+    }
+
+    if (head_node->data == data)
+    {
+        node = head_node;
+        return 0;
+    }
+
+    if (head_node->data > data)
+    {
+        find_node_in_tree(head_node->left_child, data, node);
+    }
+    else
+    {
+        find_node_in_tree(head_node->right_child, data, node);
+    }
+
+    return 0;
+}
+
+int count_node_in_tree(TREE_NODE *&head_node)
+{
+    if (head_node == NULL)
+    {
+        return 0;
+    }
+
+    return 1+ count_node_in_tree(head_node->left_child) +count_node_in_tree(head_node->right_child);
+}
+
+void print_all_node(TREE_NODE *&head_node)
+{
+    if (head_node == NULL)
+    {
+        return ;
+    }
+    print_all_node(head_node->left_child);
+    cout << head_node->data;
+    print_all_node(head_node->right_child);
+}
+
 int main()
 {
-	return 0;
+    TREE_NODE *head =NULL;
+    insert_node_to_tree(head, 4);
+    insert_node_to_tree(head, 1);
+    insert_node_to_tree(head, 5);
+    insert_node_to_tree(head, 9);
+    insert_node_to_tree(head, 0);
+
+    print_all_node(head);
+    cout << count_node_in_tree(head) <<endl;
+    return 0;
 }
